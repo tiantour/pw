43804f8be0b4137ae7d63140fd186eef68a929c2
姜灏 <150haomengting@163.com>
2017-05-04 11:09:41 +0800

    add pwd

--- pw.go
+++ pw.go
@@ -14,8 +14,6 @@ type Options struct {
 }
 
 // Cmd send data to api.pullword.com
-// date 2016-12-31
-// author andy.jiang
 func Cmd(method, source string, args Options) ([]byte, error) {
 	url := fmt.Sprintf(
 		"http://api.pullword.com/%s.php?source=%s&param1=%d&param2=%d",
